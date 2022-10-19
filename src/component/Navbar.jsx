60edd0465cd10af67ab7e9d6345b1aab30c4ebca
Mac <mac@Mac.local>
2022-10-19 16:22:44 +0200

    creating mobile menu (toggle side bar menu using useState)

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,13 +8,15 @@ import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs';
 import { TbTruckDelivery } from 'react-icons/tb';
 import { MdFavorite, MdHelp } from 'react-icons/md';
 import { FaUserFriends, FaWallet } from 'react-icons/fa';
+import { useState } from 'react';
 export default function Navbar() {
+    const [nav,setNav]=useState(false)
   return (
     //   we used [] to put our own px
     <div className=' max-w-[1640px] mx-auto flex justify-between items-center p-4 shadow'>
       {/* left side */}
       <div className='flex items-center'>
-        <div className='cursor-pointer '>
+        <div onClick={() => setNav(!nav)} className='cursor-pointer '>
           <AiOutlineMenu size={30} />
         </div>
         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
@@ -41,10 +43,22 @@ export default function Navbar() {
       </button>
       {/* mobile menu */}
       {/* overlay  */}
-      <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0 '></div>
+      {nav ? (
+        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0 '></div>
+      ) : (
+        ''
+      )}
+
       {/* Side drawer menu */}
-      <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 '>
-        <AiOutlineCloseCircle
+      <div
+        className={
+          nav
+            ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 '
+            : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300 '
+        }
+      >
+              <AiOutlineCloseCircle
+                  onClick={()=>setNav(!nav)}
           size={30}
           className='absolute right-4 top-4  cursor-pointer'
         />
