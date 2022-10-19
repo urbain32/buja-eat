4d763cfa4ddf35e2a5ebe8849142800093d9794f
Mac <mac@Mac.local>
2022-10-19 16:09:13 +0200

    creating mobile menu (side drawer menu)

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,13 @@
-import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
-import { BsFillCartFill } from 'react-icons/bs';
+import {
+  AiFillTags,
+  AiOutlineCloseCircle,
+  AiOutlineMenu,
+  AiOutlineSearch,
+} from 'react-icons/ai';
+import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs';
+import { TbTruckDelivery } from 'react-icons/tb';
+import { MdFavorite, MdHelp } from 'react-icons/md';
+import { FaUserFriends, FaWallet } from 'react-icons/fa';
 export default function Navbar() {
   return (
     //   we used [] to put our own px
@@ -31,6 +39,44 @@ export default function Navbar() {
         <BsFillCartFill size={20} className='mr-2' />
         Cart
       </button>
+      {/* mobile menu */}
+      {/* overlay  */}
+      <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0 '></div>
+      {/* Side drawer menu */}
+      <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 '>
+        <AiOutlineCloseCircle
+          size={30}
+          className='absolute right-4 top-4  cursor-pointer'
+        />
+        <h2 className='text-2xl p-4'>
+          Buja <span className='font-bold'>Eats</span>
+        </h2>
+        <nav>
+          <ul className='flex flex-col p-4 text-gray800 '>
+            <li className='text-xl py-4 flex '>
+              <TbTruckDelivery size={25} className='mr-4' /> Orders{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <MdFavorite size={25} className='mr-4' /> Favorites{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <FaWallet size={25} className='mr-4' /> Wallet{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <MdHelp size={25} className='mr-4' /> Help{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <AiFillTags size={25} className='mr-4' /> Promotions{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <BsFillSaveFill size={25} className='mr-4' /> Best Ones{' '}
+            </li>
+            <li className='text-xl py-4 flex '>
+              <FaUserFriends size={25} className='mr-4' /> Invite Friends{' '}
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
