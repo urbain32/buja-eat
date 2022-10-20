c183a3d285509d04658cf8835004ebc321ce9c2a
Mac <mac@Mac.local>
2022-10-20 11:15:32 +0200

    Creating dtabase data.js

---
+++ src/data/data.js
@@ -0,0 +1,181 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Castago',
+    category: 'red wine',
+    image:
+      'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+    price: '$$$$',
+  },
+  {
+    id: 2,
+    name: 'Nebla',
+    category: 'red wine',
+    image:
+      'https://images.unsplash.com/photo-1613313480130-6839673b6a84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+    price: '$',
+  },
+  {
+    id: 3,
+    name: 'Baco Noir',
+    category: 'red wine',
+    image:
+      'https://images.unsplash.com/photo-1582673937754-8d0cfed5dcc9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80',
+    price: '$$',
+  },
+  {
+    id: 4,
+    name: 'Ondule',
+    category: 'red wine',
+    image:
+      'https://images.unsplash.com/photo-1609951651973-03b4c838b0d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80',
+    price: '$$$',
+  },
+  {
+    id: 5,
+    name: 'Surely',
+    category: 'white wine',
+    image:
+      'https://images.unsplash.com/photo-1624364749038-8911ecc10492?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1292&q=80',
+    price: '$$',
+  },
+  {
+    id: 6,
+    name: 'Pinot Grigio',
+    category: 'white wine',
+    image:
+      'https://images.unsplash.com/photo-1579094237272-6f4a2bd85eef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80',
+    price: '$$$',
+  },
+  {
+    id: 7,
+    name: 'Sauvignon Blanc',
+    category: 'white wine',
+    image:
+      'https://images.unsplash.com/photo-1557682204-e53b55fd750c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1274&q=80',
+    price: '$$$$',
+  },
+  {
+    id: 8,
+    name: 'Palacio de Menadi',
+    category: 'white wine',
+    image:
+      'https://images.unsplash.com/photo-1572455020971-e24db68ad4b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1320&q=80',
+    price: '$',
+  },
+  {
+    id: 9,
+    name: 'The rosebae',
+    category: 'rose wine',
+    image:
+      'https://images.unsplash.com/photo-1616186835592-8c3d73c6e3a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+    price: '$$',
+  },
+  {
+    id: 10,
+    name: '20 19',
+    category: 'rose wine',
+    image:
+      'https://images.unsplash.com/photo-1588982766898-4a826e5ef631?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+    price: '$$$',
+  },
+  {
+    id: 11,
+    name: 'Rose',
+    category: 'rose wine',
+    image:
+      'https://images.unsplash.com/photo-1614208406223-5b78888e8b4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80',
+    price: '$$$$',
+  },
+  {
+    id: 12,
+    name: 'La vieille ferme',
+    category: 'rose wine',
+    image:
+      'https://images.unsplash.com/photo-1630157086556-bb93b2d8e395?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
+    price: '$',
+  },
+  {
+    id: 13,
+    name: 'Cremant de Bourgogne',
+    category: 'champagne',
+    image:
+      'https://images.unsplash.com/photo-1580410951971-e393e7b711d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80',
+    price: '$$',
+  },
+  {
+    id: 14,
+    name: 'Moët & Chandon',
+    category: 'champagne',
+    image:
+      'https://images.unsplash.com/photo-1622040787393-6b18a541ac83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
+    price: '$$$$',
+  },
+  {
+    id: 15,
+    name: 'Dom Perigon',
+    category: 'champagne',
+    image:
+      'https://images.unsplash.com/photo-1581775120934-9851ea6cf3e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+    price: '$',
+  },
+  {
+    id: 16,
+    name: 'Veuve Cliequot',
+    category: 'champagne',
+    image:
+      'https://images.unsplash.com/photo-1592341687356-7a83335fc8ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1274&q=80',
+    price: '$$$',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: 'Red Wine',
+    image:
+      'https://images.unsplash.com/photo-1606655762823-63f1cda5431f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+  },
+  {
+    id: 2,
+    name: 'White Wine',
+    image:
+      'https://e7.pngegg.com/pngimages/85/919/png-clipart-white-wine-red-wine-chardonnay-sauvignon-blanc-continental-white-wine-glass-wine-glass.png',
+  },
+  {
+    id: 3,
+    name: 'Rose Wine',
+    image:
+      'https://www.nicepng.com/png/detail/119-1191456_rose-wine-glass-png.png',
+  },
+  {
+    id: 4,
+    name: 'Champagne',
+    image:
+      'https://www.pngitem.com/pimgs/m/84-840421_champagne-clipart-7-clip-art-bottle-of-champagne.png',
+  },
+  {
+    id: 5,
+    name: 'Latest Deals',
+    image:
+      'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+  },
+  {
+    id: 6,
+    name: 'Restaurant Rewards',
+    image:
+      'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+  },
+  {
+    id: 7,
+    name: 'Best Overall',
+    image:
+      'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+  },
+  {
+    id: 8,
+    name: 'Shipped Free',
+    image:
+      'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+  },
+];
